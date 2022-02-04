d4c628395d67e8dfb4161adb9c3df0b8fd188fb4
Mikko Jed Tan <89184826+mjt1400@users.noreply.github.com>
2022-02-04 16:29:59 +0800

    Add files via upload

--- apiget.js
+++ apiget.js
@@ -1,50 +1,28 @@
-console.log("hello");
-const api_url = "https://covid19-api-philippines.herokuapp.com/api/top-regions";
-
-
-
-async function func(){
-const response = await fetch(api_url);
-const data = await response.json();
-console.log(data);
-
-var outputHtml = "";
-
-
-
-outputHtml += "<table border = 1>";
-outputHtml += "<tr>";
-outputHtml += "<th>Regions</th>";
-outputHtml += "<th>Cases</th>";
-outputHtml += "<th>Recoveries</th>";
-outputHtml += "<th>Deaths</th>";
-outputHtml += "</tr>"
-
-
-for(var i = 0; i < 18; i++){
-    outputHtml += "<tr>";
-    outputHtml += "<td>" + data['data'][i]['region']; + "</td>"
-    outputHtml += "<td>" + data['data'][i]['cases']; + "</td>"
-    outputHtml += "<td>" + data['data'][i]['recovered']; + "</td>"
-    outputHtml += "<td>" + data['data'][i]['deaths']; + "</td>"
-    outputHtml += "</tr>";
-
-   
-
+window.onload = function() {
+	getCovidStats();
 }
 
+function getCovidStats() {
+	fetch('https://coronavirus-tracker-api.herokuapp.com/v2/locations/212')
+	.then(function(resp) { return resp.json() })
+	.then(function(data) {
+		let population = data.location.country_population;
+		let update = data.location.last_updated;
+		let confirmedCases = data.location.latest.confirmed;
+		let deaths = data.location.latest.deaths;
+
+		document.getElementById('population').innerHTML = population.toLocaleString('en');
+		document.getElementById('update').innerHTML = update.substr(0, 10);
+		document.getElementById('cases').innerHTML = confirmedCases.toLocaleString('en');
+		document.getElementById('deaths').innerHTML = deaths.toLocaleString('en');
+		document.getElementById('percent').innerHTML = ((Number(deaths)/Number(confirmedCases))*100).toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2}) + "%";
 
 
 
 
-
-outputHtml += "</table>"
-
-
-
-document.getElementById("outp").innerHTML = outputHtml;
-
+	})
+	.catch(function() {
+		console.log("error");
+	})
+	setTimeout(getCovidStats, 43200000) 
 }
-func()
-
-
